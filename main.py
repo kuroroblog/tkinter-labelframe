df123bec2e4a6105f504954b5ce59ab828e20d06
tocchimittsu <arimagedon21@gmail.com>
2021-06-15 18:29:03 +0900

    修正

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ class Application(tk.Frame):
         # geometryについて : https://kuroro.blog/python/rozH3S2CYE0a0nB3s2QL/
         self.master.geometry("300x200")
 
-        # labelframe Widgetを作成する。
+        # Windowを親要素として、labelframe Widget(Frame)を作成する。
         # text : テキスト情報
         # width : 幅の設定
         # height : 高さの設定
-        labelframe = tk.LabelFrame(width=300, height=300, text="labelframe")
+        labelframe = tk.LabelFrame(self.master, text="labelframe", width=300, height=300)
 
         # Windowを親要素とした場合に、labelframe Widgetをどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
@@ -27,8 +27,7 @@ class Application(tk.Frame):
         # bg : 背景色の設定
         # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
         # Labelについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
-        label = tk.Label(labelframe, text="label",
-                         width=30, height=15, bg="orchid4")
+        label = tk.Label(labelframe, text="label", width=30, height=15, bg="blue")
 
         # labelFrame Widget(Frame)を親要素とした場合に、label Widgetをどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
@@ -38,6 +37,7 @@ class Application(tk.Frame):
 # Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
 if __name__ == "__main__":
     root = tk.Tk()
+
     app = Application(master=root)
     # Windowをループさせて、継続的にWindow表示させる。
     # mainloopについて : https://kuroro.blog/python/DmJdUb50oAhmBteRa4fi/
